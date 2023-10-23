29309f39288dbf7077410b33a20d781df8718e96
mostafaelneemr <mostafa.elnemr50@gmail.com>
2023-10-23 10:36:25 +0300

    fontawsome 2

--- app/Models/admin/Fontawsome.php
+++ app/Models/admin/Fontawsome.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fontawsome extends Model
 {
     use HasFactory;
-    protected $table = "Fontawsomes";
+    protected $table = "fontawsomes";
 
     protected $fillable = ['name'];
 
